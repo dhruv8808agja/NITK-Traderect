21a6122d5919f99b19ff703a8e2064560735f754
DodiyaParth <parthdodiya999@gmail.com>
2019-10-23 21:15:12 +0530

    some changes

--- Traderect/main_app/views.py
+++ Traderect/main_app/views.py
@@ -3,8 +3,7 @@ from django.http import HttpResponse
 from .models import *
 # Create your views here.
 def index(request):
-    a=[[1,e.rentid,e.price,e.pid,e.pid.pname,e.pid.owner.name] for e in Rentad.objects.all()]
-    print("Output || ",a)
+    a=[[1,e.rentid,e.price,e.pid,e.pid.pname,e.pid.owner.name,e.pid.avgrating] for e in Rentad.objects.all()]
     p={}
     for i in a:
             t=Photos.objects.select_related().filter(ownerid=i[3].pid)
@@ -12,6 +11,15 @@ def index(request):
                 print(t.first())
                 p[i[3].pid]=t.first().photofile
             else:
-                p[i[3].pid]='main_app/img/traderect.png'
+                p[i[3].pid]='main_app/img/default.jpeg'
+    b=[[0,e.sellid,e.price,e.pid,e.pid.pname,e.pid.owner.name] for e in Sellad.objects.all()]
+    for i in b:
+            a.append(i)
+            t=Photos.objects.select_related().filter(ownerid=i[3].pid)
+            if t.exists():
+                print(t.first())
+                p[i[3].pid]=t.first().photofile
+            else:
+                p[i[3].pid]='main_app/img/default.jpeg'
     context={'a':a,'p':p}
     return render(request, 'main_app/index.html',context)
