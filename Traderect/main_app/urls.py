7d828c8d689cb750570413c3b77696fa4ab2bdc9
DodiyaParth <parthdodiya999@gmail.com>
2019-10-26 14:59:52 +0530

    sign up done

--- Traderect/main_app/urls.py
+++ Traderect/main_app/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('signup/', views.signup, name='login'),
+    path('signup/', views.signup, name='signup'),
     path('product/<int:productID>/',views.product,name='product'),
     path('home/', views.index, name='index'),
     path('', views.login, name='login'),
